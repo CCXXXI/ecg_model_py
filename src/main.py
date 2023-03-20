2af838fcdde5b172c661efd84c23d59c8def6083
CCXXXI <ccxxxi@qq.com>
2023-03-21 03:26:28 +0800

    feat(report)!: no report
    一般用户大概看不懂这些术语，刚好统计起来还很麻烦，直接删掉好了

--- src/main.py
+++ src/main.py
@@ -1,21 +1,20 @@
 import numpy as np
 import torch
 from numpy.typing import NDArray
+
 from steps import get_checked_beats
 from steps import get_labelled_beats
 from steps import get_r_peaks
-from steps import get_report
 from utils import Beat
 from utils import set_models_path
 
 
-def infer(data: NDArray[float], ori_fs: int) -> tuple[list[Beat], str]:
+def infer(data: NDArray[float], ori_fs: int) -> list[Beat]:
     beats, r_peaks = get_r_peaks(data, ori_fs)
     checked_beats = get_checked_beats(beats, r_peaks)
     labelled_beats = get_labelled_beats(data, checked_beats, ori_fs)
-    report = get_report(labelled_beats)
 
-    return labelled_beats, report
+    return labelled_beats
 
 
 def main() -> None:
@@ -26,14 +25,12 @@ def main() -> None:
     set_models_path("../assets/ecg_models/models/")
 
     with torch.no_grad():
-        labelled_beats, report = infer(np.loadtxt(input_path), 125)
+        labelled_beats = infer(np.loadtxt(input_path), 125)
 
     with open(
         "../assets/ecg_models/output/labelled_beats.txt", "w", encoding="utf-8"
     ) as f:
         print(*labelled_beats, sep="\n", file=f)
-    with open("../assets/ecg_models/output/report.txt", "w", encoding="utf-8") as f:
-        f.write(report)
 
 
 if __name__ == "__main__":
